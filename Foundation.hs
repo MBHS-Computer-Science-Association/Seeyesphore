9e1d484de4c94e3b6b9f5487a4e70b2f58bfdbd9
Trevor Thai Kim Nguyen <trevortknguyen@gmail.com>
2016-09-09 13:20:42 -0500

    Add CDN support for Semantic-UI

--- Foundation.hs
+++ Foundation.hs
@@ -80,7 +80,9 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
-            addStylesheet $ StaticR css_bootstrap_css
+        --  addStylesheet $ StaticR css_bootstrap_css
+	    addStylesheetRemote "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.css"
+            addScriptRemote "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.js"
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 

f005a8b7642560b082dc7890426f2c017a29e819
Sammy Shin <samshin102@gmail.com>
2016-09-20 21:18:04 -0500

    Login changes

--- Handler/LoginPage.hs
+++ Handler/LoginPage.hs
@@ -7,3 +7,16 @@ getLoginPageR = do
     defaultLayout $ do
         setTitle "Humanities Login Page"
         $(widgetFile "login")
+
+postLoginPageR :: Handler ()
+postLoginPageR = do
+    -- Needs work!
+    -- redirect SubmitR
+    -- let engPost = EnglishPost "This is my title" "This is my post."
+     myUser <- runInputPost $ User
+             <$> ireq textField "username"
+             <*> ireq textField "password"
+     maybeUser <- getBy myUser
+     case maybeUser of
+	Nothing ->
+	Just (Entity myUserId user) ->

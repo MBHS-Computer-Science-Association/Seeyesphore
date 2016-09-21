0f2139abb52cdd5cd1bd49dcdddf16dd5ea3f945
Trevor Thai Kim Nguyen <trevortknguyen@gmail.com>
2016-09-20 19:17:46 -0500

    Added dashboard authentication

--- Foundation.hs
+++ Foundation.hs
@@ -101,6 +101,8 @@ instance Yesod App where
     isAuthorized ShowcaseR _ = return Authorized
     isAuthorized BlogR _ = return Authorized
 
+    isAuthorized DashboardR _ = isAuthenticated 
+
     -- This function creates static content files in the static folder
     -- and names them based on a hash of their content. This allows
     -- expiration dates to be set far in the future without worry of

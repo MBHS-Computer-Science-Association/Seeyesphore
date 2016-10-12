627f9fd560b3bf6eedac452780ec159f55d850f2
Sammy Shin <samshin102@gmail.com>
2016-10-12 13:10:56 -0500

    fixes to the dashboard

--- Handler/Dashboard.hs
+++ Handler/Dashboard.hs
@@ -4,10 +4,6 @@ import Import
 
 getDashboardR :: Handler Html
 getDashboardR = do 
-    defaultLayout $ do
-        setTitle "About STEM Humanities"
-        $(widgetFile "dashboard")
-
     posts <- runDB $ selectList [] []
     defaultLayout $ do
         setTitle "Dashboard"
